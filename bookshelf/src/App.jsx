0652eac9def34791f8610fb6f8f110247ecccd34
simplysaadali <ali091g@gmail.com>
2026-07-28 15:39:00 +0500

    Created the REACT project and now changing and writng components and code

---
+++ bookshelf/src/App.jsx
@@ -0,0 +1,20 @@
+// import { useState } from "react";
+import "./App.css"
+
+
+function App(){
+  return (
+    <div className="container">
+      <div className="header">
+        <h1 style={{color:"black", fontSize: "30px"}}>My BookShelf</h1>
+        <p style={{fontSize: "15px"}}>Book Reading Tracker</p>
+      </div>
+
+      <div className="addBook">
+        
+      </div>
+    </div>
+  )
+}
+
+export default App;
