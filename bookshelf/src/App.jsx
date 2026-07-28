ffaf42135538ef1c3bc82effb355c21f8cd75fcf
simplysaadali <ali091g@gmail.com>
2026-07-28 16:46:51 +0500

    Designned the Add Book section

--- bookshelf/src/App.jsx
+++ bookshelf/src/App.jsx
@@ -1,18 +1,19 @@
-// import { useState } from "react";
-import "./App.css"
+import { useState } from "react";
+import "./App.css";
+import BookForm from "./components/BookForm";
 
 
 function App(){
+  const [books, setBooks] = useState([]);
+
   return (
     <div className="container">
       <div className="header">
-        <h1 style={{color:"black", fontSize: "30px"}}>My BookShelf</h1>
-        <p style={{fontSize: "15px"}}>Book Reading Tracker</p>
+        <h1 style={{color:"white", fontSize: "30px"}}>My BookShelf</h1>
+        <p style={{fontSize: "15px", color:"white"}}>Book Reading Tracker</p>
       </div>
 
-      <div className="addBook">
-        
-      </div>
+      <BookForm />
     </div>
   )
 }
--- bookshelf/src/components/BookForm.jsx
+++ bookshelf/src/components/BookForm.jsx
@@ -1,1 +1,25 @@
-// import { useState } from "react";
+import "../App.css"
+
+function BookForm(){
+    return(
+       <div className="BookForm">
+            <div className="upperForm">
+                <h4>Title</h4>
+                <h4>Author</h4>
+                <h4>Pages</h4>
+            </div>
+
+            <div className="lowerForm">
+                <input type="text" placeholder="Book title"/>
+
+                <input type="text" placeholder="Author name"/>
+
+                <input type="number" placeholder="Pages"/>
+
+                <button>Add Book</button>
+            </div>
+       </div>
+    )
+}
+
+export default BookForm;
